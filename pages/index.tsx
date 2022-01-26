8e9257c61e6a114b8a1129161553fa8a89c13219
northfacegawd <chuck5732590@gmail.com>
2022-01-26 23:09:19 +0900

    [FIX - eslint]: eslint rules setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import React from 'react';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = function Home() {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
